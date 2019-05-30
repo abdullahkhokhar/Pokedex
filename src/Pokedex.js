60754362df1cb7cd343c8afca82a5794f074ac82
Abdullah Khokhar <abdullah.khokhar@mail.utoronto.ca>
2019-05-30 13:22:42 -0400

    improvement on style and design

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -19,9 +19,12 @@ class Pokedex extends Component{
   render(){
     return(
       <div className="Pokedex">
-        {this.props.pokemon.map((p) => (
-          <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
-        ))}
+        <h1>Pokedex!</h1>
+        <div className="Pokedex-cards">
+          {this.props.pokemon.map((p) => (
+            <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
+          ))}
+        </div>
       </div>
     );
   }
