c0f5863e80e9f49981aae7b3fb73b3ed70b7821c
Abdullah Khokhar <abdullah.khokhar@mail.utoronto.ca>
2019-05-30 11:49:30 -0400

    pokedex is completed, now working on styling and flexbox

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react'
 import './Pokecard.css'
-const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
 class Pokecard extends Component{
   render(){
-    let imgSrc = `${POKE_API}00${this.props.id}.png`
+    let imgSrc = `${POKE_API}${this.props.id}.png`
     return(
       <div className = "Pokecard">
         <h1>{this.props.name}</h1>
